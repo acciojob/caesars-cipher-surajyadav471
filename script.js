47d99179ad363d424c42fd5d208446f06d8f2bf3
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-23 18:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Your Script here.
 
-const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
   'I': 'V','J': 'W','K': 'X','L': 'Y',
@@ -10,11 +9,21 @@ const lookup = {
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+function rot13(encodedStr) {
+  var words = encodedStr.split(" "); // String to Array
+  let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return ;//return decodedArr
+  for (let i = 0; i < words.length; i++) { // itrat all the word & going decoded
+    const word = words[i];// for giving word string
+    let decoded_word = "";
+    for (let j = 0; j < word.length; j++) {
+      var char = word.charAt(j);
+      var decoded_char = lookup[char];
+      decoded_word += decoded_char;
+    }
+    decodedArr.push(decoded_word);
+  }
+  return decodedArr.join(" ");// join my all decoded word in my single data
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
